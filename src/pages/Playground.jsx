eb7436d683c890852d22a928b57c9d8388825edf
mern-mechanic <abhishek.kumar2526@gmail.com>
2025-09-21 14:06:25 +0530

    reconfigured tailwind and daisy UI in the system

--- src/pages/Playground.jsx
+++ src/pages/Playground.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Playground = () => {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        const localStorageValue = parseInt(localStorage.getItem("detail")) || 0;
+        const localStorageValue = parseInt(localStorage.getItem('detail')) || 0;
         console.log(typeof localStorageValue);
         setCount(localStorageValue);
     }, []);
@@ -12,22 +12,66 @@ const Playground = () => {
     const addNumber = () => {
         const val = count + 1;
         setCount(val);
-        localStorage.setItem("detail", val);
+        localStorage.setItem('detail', val);
     };
 
     const subsNumber = () => {
         const val = count - 1;
         setCount(val);
-        localStorage.setItem("detail", val);
+        localStorage.setItem('detail', val);
     };
 
     const resetNumber = () => {
         setCount(0);
-        localStorage.setItem("detail", 0);
+        localStorage.setItem('detail', 0);
     };
 
     return (
         <div>
+            <div className="carousel rounded-box">
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+                        alt="Burger"
+                    />
+                </div>
+                <div className="carousel-item">
+                    <img
+                        src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+                        alt="Burger"
+                    />
+                </div>
+            </div>
             <p className="text-5xl mb-9 flex justify-center">{count}</p>
             <div className="flex justify-center">
                 <button
